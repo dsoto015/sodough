77a746bfbc2768cbe0cf41610b993d88f131ad5b
dsoto015 <dsoto015@yahoo.com>
2026-05-02 11:22:35 -0700

    added routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,15 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './features/home/home.component';
+import { MenuComponent } from './features/menu/menu.component';
+import { GalleryComponent } from './features/gallery/gallery.component';
+import { ReviewsComponent } from './features/reviews/reviews.component';
+import { OrderComponent } from './features/order/order.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
+  { path: 'menu', component: MenuComponent }, 
+  { path: 'gallery', component: GalleryComponent }, 
+  { path: 'reviews', component: ReviewsComponent }, 
+  { path: 'order', component: OrderComponent }, 
   { path: '**', redirectTo: '' }
 ];
